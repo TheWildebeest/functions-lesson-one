8cf2c3be2e7657da375d6acaf8909dba1283a35e
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-01 11:09:10 +0100

    created basic function lesson two

---
+++ 02-check-age.js
@@ -0,0 +1,64 @@
+say("Age: 17. Can purchase alcohol: " + canPurchaseAlcohol(17) + "\n")
+// expect "No!"
+
+say("Age: 21. Can purchase alcohol: " + canPurchaseAlcohol(21) + "\n");
+// expect "Yes!"
+
+say("Age: 18. Can purchase alcohol: " + canPurchaseAlcohol(18, "USA") + "\n");
+// expect "No!"
+
+say("Age:  ?  Can purchase alcohol: " + canPurchaseAlcohol("Sudan") + "\n");
+// expect "No!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+function canPurchaseAlcohol(age, country) {
+  if (age < 21 && country === "USA") {
+    return "No!";
+  } else if (age < 18) {
+    return "No!";
+  } else {
+    return "Yes!";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function say(string) {
+  console.log(string);
+}
